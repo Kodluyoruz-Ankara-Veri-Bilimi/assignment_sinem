18985d574bac9af2897d3a9106224e04f913a6f2
sinemsenel <62889994+sinemsenel@users.noreply.github.com>
2020-07-10 23:19:35 +0300

    Add files via upload

---
+++ class homework.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as ps
+import seaborn as sns
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Information():
+    def __init__(self):
+        print(....)
+    def dataset = pd.read_csv('file_name')    
+    def data.head() 
+    def info(...)
+       pass
+    def corr = self.corr()
+       pass
+    def corr.style.background_gradient(cmap='coolwarm')
+      pass
+
+    
+class PreProcessStrategy():
+    def __init__(self):
+        
+    def fillna(self, data,..)
+        pass
+    def _get_dummies(self, data,...)
+        pass
+        
+    def outlier_describing(self):
+        pass
+    def standardisation(self):
+        pass
+    #spliting data test and train
+    def  y=data.labelname
+      pass
+    def x=data.drop('labelname',axis=1)
+      pass
+    def  x_train, x_test, y_train,y_test=train_test_split(x,y,train_size=..., test_size=..., random_state=123) 
+      pass
+    
+class Visualizer:
+    def __init__(self):
+        
+    def scatterplot(self,data, x_label="", y_label="", title="", color = "", yscale_log=False)
+       pass
+    def boxplot(self,)
+      pass
+    def hist(self,)
+      pass
+    def heatmap(self,)
+    pass
+      
+
